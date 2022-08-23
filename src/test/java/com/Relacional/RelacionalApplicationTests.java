d1b9ee59a733b9a3ec97467a84f6eb01c93fd7fd
Raphael Bovi <93729728+RaphaelBovi@users.noreply.github.com>
2022-08-23 19:04:23 -0300

    first commit

---
+++ src/test/java/com/Relacional/RelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Relacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
